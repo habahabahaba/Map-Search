ff8c53126479c3072947d1067921d629d1bed2f9
Alex S <habahabahaba@outlook.com>
2024-09-12 02:24:31 +0400

    Finished implementing searchLocations query function.

--- src/api/searchLocations.ts
+++ src/api/searchLocations.ts
@@ -3,6 +3,15 @@ import { Place } from './Place.type';
 interface SearchOptionsObj {
   limit?: number;
 }
+interface SearchLocationsResponse {
+  features: {
+    geometry: { coordinates: number[] };
+    properties: {
+      place_id: number;
+      display_name: string;
+    };
+  }[];
+}
 
 const BASE_URL: string = 'https://nominatim.openstreetmap.org/search?';
 const searchOptions = (searchOptions: SearchOptionsObj) =>
@@ -11,9 +20,22 @@ const searchOptions = (searchOptions: SearchOptionsObj) =>
   }`;
 
 // Query function:
-export default async function searchLocations(term: string) {
-  const res = await fetch(`${BASE_URL}q=${term}&${searchOptions({})}`);
-  const data = await res.json();
+export default async function searchLocations(
+  term: string,
+  SearchOptionsObj: SearchOptionsObj = {}
+) {
+  const res = await fetch(
+    `${BASE_URL}q=${term}&${searchOptions(SearchOptionsObj)}`
+  );
+  const data: SearchLocationsResponse = await res.json();
+  //   console.log(data);
 
-  console.log(data);
+  const places: Place[] = data.features.map(({ geometry, properties }) => ({
+    id: properties.place_id,
+    name: properties.display_name,
+    longitude: geometry.coordinates[0],
+    latitude: geometry.coordinates[1],
+  }));
+
+  return places;
 }
