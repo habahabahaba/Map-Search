fdba3f53f1500b58572d18cdb57f9f51d4ad8e54
Alex S <habahabahaba@outlook.com>
2024-09-11 05:02:30 +0400

    Add api directory; define Place type.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+// React:
+import { useState } from 'react';
 // Components:
 import LocationSearch from './Components/LocationSearch';
 import Map from './Components/Map';
 // CSS:
 
+// Types, interfaces and enumns:
+import type { Place } from './api/Place.type';
+
 function App() {
+  // State:
+  const [place, setPlace] = useState<Place | null>(null);
+
+  // Handlers:
+  function handleSearch() {}
+
+  // JSX:
   return (
     <div className='h-screen w-screen grid grid-cols-12'>
       <div className='col-span-3 p-2'>
---
+++ src/api/Place.type.ts
@@ -0,0 +1,7 @@
+// Types, interfaces and enumns:
+export interface Place {
+  id: number;
+  name: string;
+  longitude: number;
+  latitude: number;
+}
