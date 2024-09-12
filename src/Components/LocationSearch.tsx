a99483cdaf5c9843a7c33a15f0cedcc5cbc67516
Alex S <habahabahaba@outlook.com>
2024-09-12 05:45:12 +0400

    Add renderedPlaces to the LocationSearch.jsx.

--- src/Components/LocationSearch.tsx
+++ src/Components/LocationSearch.tsx
@@ -1,8 +1,10 @@
 // 3rd party:
+// Query function:
+import searchLocations from '../api/searchLocations';
 // Redux RTK:
 // Store:
 // React:
-import { useState, useRef } from 'react';
+import { useState, useRef, Fragment } from 'react';
 // Context:
 // Components:
 // CSS:
@@ -30,12 +32,28 @@ const LocationSearch: FC<LocationSearchProps> = ({ handlePlaceClick }) => {
 
     // Debouncing API calls:
     if (submitTimeoutRef.current) clearTimeout(submitTimeoutRef.current);
-    submitTimeoutRef.current = setTimeout(() => {
-      console.log(`Calling search API with the search term: ${term}`);
+    submitTimeoutRef.current = setTimeout(async () => {
+      // console.log(`Calling search API with the search term: ${term}`);
+      const results = await searchLocations(term);
+      // console.log(response);
+      setPlaces(() => results);
     }, 800);
   }
 
   // JSX:
+  const renderedPlaces = places.map((place) => (
+    <Fragment key={place.id}>
+      <p className='text-sm'>{place.name}</p>
+      <button
+        onClick={() => handlePlaceClick(place)}
+        className='bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded'
+      >
+        Go
+      </button>
+      <div className='border-b w-full col-span-2' />
+    </Fragment>
+  ));
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -50,8 +68,17 @@ const LocationSearch: FC<LocationSearchProps> = ({ handlePlaceClick }) => {
           id='term'
           className='border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full'
         />
-        <button type='submit'>Search</button>
+        <button
+          type='submit'
+          className='bg-blue-500 text-s text-white font-bold py-1 px-1 mt-1 rounded'
+        >
+          Search
+        </button>
       </form>
+      <h1 className='font-bold mt-6'>Found Locations:</h1>
+      <div className='grid grid-cols-[1fr_40px] gap-2 mt-2 items-center '>
+        {renderedPlaces}
+      </div>
     </div>
   );
 };
