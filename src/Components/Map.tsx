baac55ceb125b56b58429bc3c6d1de08ad9111d6
Alex S <habahabahaba@outlook.com>
2024-09-12 06:05:28 +0400

    Add imports to Map.jsx

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,12 +1,18 @@
 // 3rd party:
+// Leaflet:
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 // Redux RTK:
 // Store:
 // React:
+import { useEffect, useRef } from 'react';
 // Context:
 // Components:
 // CSS:
+import 'leaflet/dist/leaflet.css';
+
 // Types, interfaces and enumns:
 import type { FC } from 'react';
+import type { Map as LeafletMap } from 'leaflet';
 import type { Place } from '../api/Place.type';
 
 interface MapProps {
